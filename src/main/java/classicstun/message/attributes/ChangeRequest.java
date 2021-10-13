a7bafda5c2d1451149c936520294f98dc3bf533b
jiangsenwei <jiangsenwei@star-net.cn>
2021-10-13 14:22:41 +0800

    ChangeRequest 实现decode方法

--- src/main/java/classicstun/message/attributes/ChangeRequest.java
+++ src/main/java/classicstun/message/attributes/ChangeRequest.java
@@ -1,18 +1,50 @@
 package classicstun.message.attributes;
 
 import classicstun.message.enums.MessageAttributeType;
+import classicstun.message.exception.MessageAttributeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 /**
- * @author JiangSenwei
+ * @author JiangZhenli
+ * <p>
+ * 0                   1                   2                   3
+ * 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 A B 0|
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * <p>
+ * A: This is the "change IP" flag.  If true, it requests the server
+ * to send the Binding Response with a different IP address than the
+ * one the Binding Request was received on.
+ * <p>
+ * B: This is the "change port" flag.  If true, it requests the
+ * server to send the Binding Response with a different port than the
+ * one the Binding Request was received on.
  */
 public class ChangeRequest extends MessageAttribute {
 
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
+    private final static int FF = 0; // 0000
+    private final static int FT = 2; // 0010
+    private final static int TF = 4; // 0100
+    private final static int TT = 6; // 0110
+    private final static List<Integer> TruthValues = List.of(FF, FT, TF, TT);
+
+    private boolean changeIp = false;
+    private boolean changePort = false;
+
     ChangeRequest() {
         super(MessageAttributeType.CHANGE_REQUEST);
+    }
+
+    public ChangeRequest(boolean changeIp, boolean changePort) {
+        super(MessageAttributeType.CHANGE_REQUEST);
+        this.changeIp = changeIp;
+        this.changePort = changePort;
     }
 
     @Override
@@ -21,7 +53,39 @@ public class ChangeRequest extends MessageAttribute {
     }
 
     @Override
-    public void decode(byte[] attributeData) {
+    public void decode(byte[] attributeData) throws MessageAttributeException {
+        if (attributeData.length != 4) {
+            throw new MessageAttributeException("ChangeRequest Attribute Length Error.");
+        }
+        if (attributeData[0] != 0 ||
+                attributeData[1] != 0 ||
+                attributeData[2] != 0 ||
+                !TruthValues.contains((int) attributeData[3])
+        ) {
+            throw new MessageAttributeException("Unrecognized ChangeRequest Attribute.");
+        }
 
+        switch ((int) attributeData[3]) {
+            case FT:
+                changePort = true;
+                break;
+            case TF:
+                changeIp = true;
+                break;
+            case TT:
+                changePort = true;
+                changeIp = true;
+            default:break;
+        }
+
+        this.setValue(attributeData);
+    }
+
+    public boolean isChangeIp() {
+        return changeIp;
+    }
+
+    public boolean isChangePort() {
+        return changePort;
     }
 }
