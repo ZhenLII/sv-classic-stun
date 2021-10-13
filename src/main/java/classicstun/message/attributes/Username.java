ce192f845ac5bfc537f8996e8db5f8facfdf6aa1
jiangsenwei <jiangsenwei@star-net.cn>
2021-10-13 15:01:52 +0800

    decode方法实现

--- src/main/java/classicstun/message/attributes/Username.java
+++ src/main/java/classicstun/message/attributes/Username.java
@@ -1,11 +1,12 @@
 package classicstun.message.attributes;
 
 import classicstun.message.enums.MessageAttributeType;
+import classicstun.message.exception.MessageAttributeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * @author JiangSenwei
+ * @author JiangZhenli
  */
 public class Username extends MessageAttribute {
 
@@ -15,13 +16,20 @@ public class Username extends MessageAttribute {
         super(MessageAttributeType.USERNAME);
     }
 
+    private String username;
+
     @Override
     public byte[] encode() {
         return new byte[0];
     }
 
     @Override
-    public void decode(byte[] attributeData) {
+    public void decode(byte[] attributeData) throws MessageAttributeException {
+        if(attributeData.length <= 0 || attributeData.length % 4 != 0) {
+            throw new MessageAttributeException("Username Length Error.");
+        }
 
+        this.username = new String(attributeData);
+        this.setValue(attributeData);
     }
 }
